138d4c680f05d56a6c6240ec72330122470ed625
Bruno Staine <brunostaine@hotmail.com>
2023-09-03 15:12:38 -0300

    Add angular material

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # App Heroes
 
-# Quality of the Code
+## Quality of the Code
 
 [![DeepScan grade](https://deepscan.io/api/teams/22059/projects/25413/branches/794690/badge/grade.svg)](https://deepscan.io/dashboard#view=project&tid=22059&pid=25413&bid=794690)
+
+## Bibliotecas
+
+- Angular Material: ng add @angular/material
+- Flex Layout: npm i @angular/flex-layout
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,19 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
-  ],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
