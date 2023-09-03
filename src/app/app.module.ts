56ec76c4a65f7369ba96761176d1e6d8d779e940
Bruno Staine <brunostaine@hotmail.com>
2023-09-03 18:33:10 -0300

    Refactor: Enviando imports do material para o modulo do material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MaterialModule } from './material/material.module';
 import { MessagesComponent } from './messages/messages.component';
-import { MatListModule } from '@angular/material/list';
-import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -33,16 +25,8 @@ import { MatTableModule } from '@angular/material/table';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule,
     FlexLayoutModule,
-    MatTooltipModule,
-    MatCardModule,
-    MatListModule,
-    MatInputModule,
-    MatTableModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
